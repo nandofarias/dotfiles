69d558d0511805d574277fce14ce330781a28e60
Fernando Farias <nandofarias@me.com>
2021-10-11 10:21:32 -0300

    add kitty shortcut command .

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,3 +7,12 @@
   end
 end)
 
+hs.hotkey.bind({"command"}, ".", function()
+  app = hs.application.get('kitty')
+  if app ~= nil and app:isFrontmost() then
+    app:hide()
+  else
+    hs.application.launchOrFocus("/Applications/Kitty.app")
+  end
+end)
+
