8a7064e44405f700a89eb0678bfe6caf14474df5
Fernando Farias <nandofarias@me.com>
2021-10-11 17:34:06 -0300

    refactor to use basename

--- .config/fish/functions/tmux.fish
+++ .config/fish/functions/tmux.fish
@@ -1,7 +1,7 @@
 function tn --description 'Creates a new tmux session'
-    test $argv; and tmux new -s $argv; or tmux new -s (pwd | sed 's/.*\///g')
+    test $argv; and tmux new -s $argv; or tmux new -s (basename (pwd))
 end
 
 function ta --description 'Creates a new tmux session'
-    test $argv; and tmux attach -t $argv; or tmux attach -t (pwd | sed 's/.*\///g')
+    test $argv; and tmux attach -t $argv; or tmux attach -t (basename (pwd))
 end
