51508598f250063e104077c36c8cf39820ef9f78
Fernando Farias <nandofarias@me.com>
2022-02-17 10:20:35 -0300

    hammerspoon: add new apps shortcuts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,22 +1,19 @@
- hs.hotkey.bind({"command"}, ";", function()
-  local app = hs.application.get('Alacritty')
-  if app ~= nil and app:isFrontmost() then
-    app:hide()
-  else
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
-  end
-end)
+H = {}
+function H.openApp(appName, appPath)
+    return function()
+        local app = hs.application.get(appName)
+        if app ~= nil and app:isFrontmost() then
+            app:hide()
+        else
+            hs.application.launchOrFocus(appPath)
+        end
+    end
+end
 
-hs.hotkey.bind({"command"}, ".", function()
-  local app = hs.application.get('kitty')
-  if app ~= nil and app:isFrontmost() then
-    app:hide()
-  else
-    app = hs.application.launchOrFocus("/Applications/Kitty.app")
-    local win = hs.window.focusedWindow()
-    local screen = win:screen()
-
-    win:centerOnScreen(screen, true, 0)
-  end
-end)
-
+hs.hotkey.bind({ "command" }, ";", H.openApp("Alacritty", "/Applications/Alacritty.app"))
+hs.hotkey.bind({ "command" }, ".", H.openApp("kitty", "/Applications/Kitty.app"))
+-- Ctrl + 1,2,3 is used to switch to different desktops
+hs.hotkey.bind({ "control" }, "4", H.openApp("Safari", "/Applications/Safari.app"))
+hs.hotkey.bind({ "control" }, "5", H.openApp("Dash", "/Applications/Dash.app"))
+hs.hotkey.bind({ "control" }, "6", H.openApp("Slack", "/Applications/Slack.app"))
+hs.hotkey.bind({ "control" }, "7", H.openApp("WhatsApp", "/Applications/WhatsApp.app"))
