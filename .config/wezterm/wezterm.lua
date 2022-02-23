077cd095e4647b7119d117a3a6335354a0e63a80
Fernando Farias <nandofarias@me.com>
2022-02-23 11:47:27 -0300

    initial version wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("DM Mono"),
+  color_scheme = "Dracula",
+  font_size = 15,
+  line_height = 1.2,
+  window_decorations = "NONE",
+  window_close_confirmation = "NeverPrompt",
+  window_background_opacity = 0.7,
+}
