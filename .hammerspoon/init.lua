1473d607a05066decd71df294d2badbb9e99d505
Fernando Farias <nandofarias@me.com>
2022-04-05 11:14:51 -0300

    add filewatcher to hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,19 +1,17 @@
-H = {}
-function H.openApp(appName, appPath)
-    return function()
-        local app = hs.application.get(appName)
-        if app ~= nil and app:isFrontmost() then
-            app:hide()
-        else
-            hs.application.launchOrFocus(appPath)
-        end
+local function reload_config(files)
+  local doReload = false
+  for _, file in pairs(files) do
+    if file:sub(-4) == '.lua' then
+      doReload = true
     end
+  end
+  if doReload then
+    hs.reload()
+  end
 end
 
-hs.hotkey.bind({ "command" }, ";", H.openApp("Alacritty", "/Applications/Alacritty.app"))
-hs.hotkey.bind({ "command" }, ".", H.openApp("kitty", "/Applications/Kitty.app"))
--- Ctrl + 1,2,3 is used to switch to different desktops
-hs.hotkey.bind({ "control" }, "4", H.openApp("Safari", "/Applications/Safari.app"))
-hs.hotkey.bind({ "control" }, "5", H.openApp("Dash", "/Applications/Dash.app"))
-hs.hotkey.bind({ "control" }, "6", H.openApp("Slack", "/Applications/Slack.app"))
-hs.hotkey.bind({ "control" }, "7", H.openApp("WhatsApp", "/Applications/WhatsApp.app"))
+-- reload config every time it changes
+local config_watcher = hs.pathwatcher.new('~/.hammerspoon/', reload_config)
+config_watcher:start()
+
+require('keymaps')
