19828941fd8b72ff92887faad0ebad8d513c0eac
Fernando Farias <nandofarias@me.com>
2022-05-04 12:13:19 -0300

    nvim: neogit split above

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,9 +1,17 @@
 local vimp = require('vimp')
 local neogit = require('neogit')
 
-neogit.setup{
+neogit.setup {
   integrations = {
     diffview = true
+  },
+  mappings = {
+    status = {
+      ['q'] = 'Close',
+      ['<esc>'] = 'Close',
+    }
   }
 }
-vimp.nnoremap('<C-g>', neogit.open)
+vimp.nnoremap('<C-g>', function()
+  neogit.open({ kind = 'split_above' })
+end)
