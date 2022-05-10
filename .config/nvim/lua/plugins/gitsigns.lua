c9dfd359b1726ab2ff909762f2b64496ba5c14b5
Fernando Farias <nandofarias@me.com>
2022-05-10 14:47:30 -0300

    nvim: tweaks in gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -4,8 +4,6 @@ require('gitsigns').setup {
   current_line_blame = true,
   current_line_blame_opts = {
     delay = 500,
-  },
-  current_line_blame_opts = {
     ignore_whitespace = true,
   },
   on_attach = function(bufnr)
@@ -18,23 +16,32 @@ require('gitsigns').setup {
     end
 
     -- navigation
-    map('n', ']c', "&diff ? ']c' : '<cmd>gitsigns next_hunk<cr>'", {expr=true})
-    map('n', '[c', "&diff ? '[c' : '<cmd>gitsigns prev_hunk<cr>'", {expr=true})
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
 
     -- actions
-    map({'n', 'v'}, '<leader>hs', ':gitsigns stage_hunk<cr>')
-    map({'n', 'v'}, '<leader>hr', ':gitsigns reset_hunk<cr>')
+    map({ 'n', 'v' }, '<leader>hs', ':gitsigns stage_hunk<cr>')
+    map({ 'n', 'v' }, '<leader>hr', ':gitsigns reset_hunk<cr>')
     map('n', '<leader>hs', gs.stage_buffer)
     map('n', '<leader>hu', gs.undo_stage_hunk)
     map('n', '<leader>hr', gs.reset_buffer)
     map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>hb', function() gs.blame_line { full = true } end)
     map('n', '<leader>tb', gs.toggle_current_line_blame)
     map('n', '<leader>hd', gs.diffthis)
     map('n', '<leader>hd', function() gs.diffthis('~') end)
     map('n', '<leader>td', gs.toggle_deleted)
 
     -- text object
-    map({'o', 'x'}, 'ih', ':<c-u>gitsigns select_hunk<cr>')
+    map({ 'o', 'x' }, 'ih', ':<c-u>gitsigns select_hunk<cr>')
   end
 }
