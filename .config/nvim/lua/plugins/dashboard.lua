efe7f3a7d8f8bc1fa585d40cc3e1be3974a48308
Fernando Farias <nandofarias@me.com>
2022-06-15 12:37:19 -0300

    nvim: update dashboard config to the newest lua version

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,15 +1,16 @@
-local vimp = require('vimp')
-vim.g.dashboard_default_executive = 'telescope'
-vim.g.dashboard_custom_section = {
-  a = { description = { '  Find File                 leader f f' }, command = 'Telescope find_files' },
-  b = { description = { '  Find Word                 leader f g' }, command = 'Telescope live_grep' },
-  c = { description = { '  Recents                   leader f h' }, command = 'Telescope oldfiles' },
-  d = { description = { '  New File                  leader c n' }, command = 'DashboardNewFile' },
-  f = { description = { '  Update Plugins            leader u  ' }, command = 'PackerSync' },
-  g = { description = { '  Settings                  leader n v' }, command = 'edit $MYVIMRC' },
-  h = { description = { '  Exit                      leader q q' }, command = 'exit' }
+local db = require('dashboard')
+db.custom_center = {
+  { icon = '   ', desc = 'Find File                      ', shortcut = 'leader f f', action = 'Telescope find_files' },
+  { icon = '   ', desc = 'Find Word                      ', shortcut = 'leader f g', action = 'Telescope live_grep' },
+  { icon = '   ', desc = 'Recents                        ', shortcut = 'leader f h', action = 'Telescope oldfiles' },
+  { icon = '   ', desc = 'New File                       ', shortcut = 'leader c n', action = 'DashboardNewFile' },
+  { icon = '   ', desc = 'Update Plugins                 ', shortcut = 'leader u  ', action = 'PackerSync' },
+  { icon = '   ', desc = 'Settings                       ', shortcut = 'leader n v', action = 'edit $MYVIMRC' },
+  { icon = '   ', desc = 'Exit                           ', shortcut = 'leader q q', action = 'exit' }
 }
-vim.g.dashboard_custom_header = {
+db.custom_header = {
+  '                                                       ',
+  '                                                       ',
   '                        ______________                 ',
   '                       /             /|                ',
   '                      /             / |                ',
@@ -29,10 +30,11 @@ vim.g.dashboard_custom_header = {
   '               |   -----------     ::: |               ',
   '               `-----------------------`               ',
   '                                                       ',
+  '                                                       ',
+  '                                                       ',
 }
-vimp.nnoremap('<silent> <Leader>fh', ':DashboardFindHistory<CR>')
-vimp.nnoremap('<silent> <Leader>ff', ':DashboardFindFile<CR>')
-vimp.nnoremap('<silent> <Leader>fg', ':DashboardFindWord<CR>')
-vimp.nnoremap('<silent> <Leader>fb', ':DashboardJumpMark<CR>')
-vimp.nnoremap('<silent> <Leader>cn', ':DashboardNewFile<CR>')
+vim.cmd [[au VimEnter * hi! link DashboardHeader DraculaYellow]]
+vim.cmd [[au VimEnter * hi! link DashboardCenter DraculaPurple]]
+vim.cmd [[au VimEnter * hi! link DashboardCenterIcon DraculaCyan]]
 vim.cmd [[au VimEnter * hi! link DashboardShortcut DraculaGreen]]
+vim.cmd [[au VimEnter * hi! link DashboardFooter DraculaPurple]]
