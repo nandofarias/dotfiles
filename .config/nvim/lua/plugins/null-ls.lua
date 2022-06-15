67035460ca05b6856dabfe90d2e3a5ba54a1a196
Fernando Farias <nandofarias@me.com>
2022-06-15 12:38:24 -0300

    nvim: using null-ls eslintd which is faster

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -2,8 +2,7 @@ local null_ls = require('null-ls')
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.credo.with({
       condition = function(utils)
