c0e06693db20f0c0e8142e9c9d7491029061c974
Fernando Farias <nandofarias@me.com>
2022-06-15 12:37:54 -0300

    nvim: fix discord presence and hide filenames and workspaces

--- .config/nvim/lua/plugins/presence.lua
+++ .config/nvim/lua/plugins/presence.lua
@@ -1,1 +1,8 @@
-require('presence'):setup()
+require('presence'):setup({
+  editing_text        = "Typing...",
+  file_explorer_text  = "Browsing...",
+  git_commit_text     = "Committing changes",
+  plugin_manager_text = "Managing plugins",
+  reading_text        = "Thinking...",
+  workspace_text      = "Working on stuff"
+})
