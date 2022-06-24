f21b0599d1b35dc9ec4f7869151cbf37742092b8
Fernando Farias <nandofarias@me.com>
2022-06-24 12:30:52 -0300

    wezterm: tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ return {
   font = wezterm.font("MonoLisa"),
   font_size = 15,
   line_height = 1.2,
+  local_echo_threshold_ms = 300,
 
   window_decorations = "RESIZE",
   window_close_confirmation = "NeverPrompt",
@@ -21,61 +22,61 @@ return {
       selection_bg = "#414d58",
       scrollbar_thumb = "#414d58",
       split = "#9580ff",
-      ansi = {"#0b0d0f", "#ff9580", "#8aff80", "#ffff80", "#9580ff", "#ff80bf", "#80ffea", "#f8f8f2"},
-      brights = {"#0b0d0f", "#ffaa99", "#a2ff99", "#ffff99", "#aa99ff", "#ff99cc", "#99ffee", "#ffffff"},
+      ansi = { "#0b0d0f", "#ff9580", "#8aff80", "#ffff80", "#9580ff", "#ff80bf", "#80ffea", "#f8f8f2" },
+      brights = { "#0b0d0f", "#ffaa99", "#a2ff99", "#ffff99", "#aa99ff", "#ff99cc", "#99ffee", "#ffffff" },
       indexed = {
-          [136] = "#414d58"
+        [136] = "#414d58"
       },
       compose_cursor = "#ff9580",
       tab_bar = {
-          background = "#0b0d0f", active_tab = {
-              bg_color = "#708ca9",
-              fg_color = "#f8f8f2",
-              intensity = "Normal",
-              underline = "None",
-              italic = false,
-              strikethrough = false
-          },
-          inactive_tab = {
-              bg_color = "#414d58",
-              fg_color = "#f8f8f2"
-          },
-          inactive_tab_hover = {
-              bg_color = "#708ca9",
-              fg_color = "#f8f8f2",
-              italic = true
-          },
-          new_tab = {
-              bg_color = "#0b0d0f",
-              fg_color = "#f8f8f2"
-          },
-          new_tab_hover = {
-              bg_color = "#ff80bf",
-              fg_color = "#f8f8f2",
-              italic = true
-          }
+        background = "#0b0d0f", active_tab = {
+          bg_color = "#708ca9",
+          fg_color = "#f8f8f2",
+          intensity = "Normal",
+          underline = "None",
+          italic = false,
+          strikethrough = false
+        },
+        inactive_tab = {
+          bg_color = "#414d58",
+          fg_color = "#f8f8f2"
+        },
+        inactive_tab_hover = {
+          bg_color = "#708ca9",
+          fg_color = "#f8f8f2",
+          italic = true
+        },
+        new_tab = {
+          bg_color = "#0b0d0f",
+          fg_color = "#f8f8f2"
+        },
+        new_tab_hover = {
+          bg_color = "#ff80bf",
+          fg_color = "#f8f8f2",
+          italic = true
+        }
       }
     }
   },
 
   keys = {
-    {key="d", mods="CMD", action=wezterm.action{SendString="\x02\x76"}},
-    {key="d", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x73"}},
-    {key="t", mods="CMD", action=wezterm.action{SendString="\x02\x63"}},
-    {key="w", mods="CMD", action=wezterm.action{SendString="\x02\x78"}},
+    { key = "d", mods = "CMD", action = wezterm.action { SendString = "\x02\x76" } },
+    { key = "d", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x73" } },
+    { key = "t", mods = "CMD", action = wezterm.action { SendString = "\x02\x63" } },
+    { key = "w", mods = "CMD", action = wezterm.action { SendString = "\x02\x78" } },
 
-    {key="[", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x70"}},
-    {key="]", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x6e"}},
+    { key = "[", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x70" } },
+    { key = "]", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x6e" } },
 
-    {key="`", mods="CTRL", action=wezterm.action{SendString="\x02\x7a"}},
+    { key = "`", mods = "CTRL", action = wezterm.action { SendString = "\x02\x7a" } },
 
-    {key="[", mods="CTRL", action=wezterm.action{SendString="\x02\x5b"}},
+    { key = "[", mods = "CTRL", action = wezterm.action { SendString = "\x02\x5b" } },
 
-    {key="h", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x3c"}},
-    {key="j", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x2d"}},
-    {key="k", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x2b"}},
-    {key="l", mods="CMD|SHIFT", action=wezterm.action{SendString="\x02\x3e"}},
+    { key = "h", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x3c" } },
+    { key = "j", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x2d" } },
+    { key = "k", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x2b" } },
+    { key = "l", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x3e" } },
 
-    {key="k", mods="CMD", action=wezterm.action{SendString="\x63\x6C\x65\x61\x72\x0A"}},
+    { key = "k", mods = "CMD", action = wezterm.action { SendString = "\x63\x6C\x65\x61\x72\x0A" } },
   },
 }
