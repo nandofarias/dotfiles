48ffa9b3cba1732525db132dd13aa9fbc0dc8d73
Fernando Farias <nandofarias@me.com>
2022-07-13 13:56:02 -0300

    nvim: modes.nvim config

--- .config/nvim/lua/plugins/modes.lua
+++ .config/nvim/lua/plugins/modes.lua
@@ -1,2 +1,28 @@
-require('modes').setup()
+-- HACK: not sure why but when vim enter it's getting gray and not colored
+vim.cmd [[ autocmd VimEnter * highlight CursorLine guibg=#201529 guifg=fg ]]
 
+require('modes').setup({
+  colors = {
+    copy = "#f5c359",
+    delete = "#c75c6a",
+    insert = "#78ccc5",
+    visual = "#9745be",
+  },
+
+  -- Set opacity for cursorline and number background
+  line_opacity = 0.15,
+
+  -- Enable cursor highlights
+  set_cursor = true,
+
+  -- Enable cursorline initially, and disable cursorline for inactive windows
+  -- or ignored filetypes
+  set_cursorline = true,
+
+  -- Enable line number highlights to match cursorline
+  set_number = true,
+
+  -- Disable modes highlights in specified filetypes
+  -- Please PR commonly ignored filetypes
+  ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
+})
