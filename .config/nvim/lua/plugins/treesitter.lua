fd2bedc25ee3eecd1a35870621e642be296ad755
Fernando Farias <nandofarias@me.com>
2022-12-22 21:09:26 -0300

    nvim: add just ts parser

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -9,3 +9,13 @@ require('nvim-treesitter.configs').setup {
     additional_vim_regex_highlighting = { "markdown" },
   },
 }
+
+require("nvim-treesitter.parsers").get_parser_configs().just = {
+  install_info = {
+    url = "https://github.com/IndianBoy42/tree-sitter-just",
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main",
+    use_makefile = true,
+  },
+  maintainers = { "@IndianBoy42" },
+}
