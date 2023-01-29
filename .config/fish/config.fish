5a3591da323b9e178e71131942879640d182714d
Fernando Farias <nandofarias@me.com>
2023-01-28 21:19:20 -0300

    nvim: use stable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -100,7 +100,7 @@ abbr -ag ss "cmatrix -C green"
 
 abbr -ag v nvim
 abbr -ag vim nvim
-abbr -ag update-nvim 'asdf uninstall neovim nightly && asdf install neovim nightly'
+abbr -ag update-nvim 'asdf uninstall neovim stable && asdf install neovim stable'
 abbr -ag plug-sync "nvim +PlugClean! +PlugUpdate +PlugInstall"
 abbr -ag vim-profile "nvim --startuptime timeCost.txt timeCost.txt; and rm timeCost.txt"
 abbr -ag lsp-logs "tail -f $HOME/.local/state/nvim/lsp.log"
