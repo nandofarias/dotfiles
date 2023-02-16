a95e56ff1e3e0296feac61903f23d2f6ce21a2cb
Fernando Farias <nandofarias@me.com>
2023-02-16 18:52:20 -0300

    nvim: persisted keymaps descriptions

--- .config/nvim/lua/plugins/persisted.lua
+++ .config/nvim/lua/plugins/persisted.lua
@@ -1,30 +1,28 @@
 return {
-  'olimorris/persisted.nvim',
-  config = function()
-    vim.keymap.set('n', '<Leader>sl', function()
-      require('persisted').load()
-    end)
+    'olimorris/persisted.nvim',
+    keys = {
+        { '<Leader>sl', function()
+          require('persisted').load()
+        end, desc = 'Save Session' },
+        { '<Leader>ss', function()
+          require('persisted').save()
+        end, desc = 'Load Session' },
+        { '<Leader>st', ':Telescope persisted<CR>', desc = "Telescope Sessions" }
+    },
+    config = function()
+      require('persisted').setup {
+          use_git_branch = true,
+          branch_separator = '@@',
+          after_save = function()
+            vim.notify('Session was saved!')
+          end,
+          telescope = {
+              before_source = function()
+                vim.cmd('bufdo Bwipeout')
+              end
+          },
+      }
 
-    vim.keymap.set('n', '<Leader>ss', function()
-      require('persisted').save()
-    end)
-
-    vim.keymap.set('n', '<Leader>st', ':Telescope persisted<CR>')
-
-
-    require('persisted').setup {
-      use_git_branch = true,
-      branch_separator = '@@',
-      after_save = function()
-        vim.notify('Session was saved!')
-      end,
-      telescope = {
-        before_source = function()
-          vim.cmd('bufdo Bwipeout')
-        end
-      },
-    }
-
-    require('telescope').load_extension('persisted')
-  end
+      require('telescope').load_extension('persisted')
+    end
 }
