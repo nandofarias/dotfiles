9d17c8bb8b769d70b320e417478854ea44e256de
Fernando Farias <nandofarias@me.com>
2023-02-26 16:44:42 -0300

    nvim: fix persisted breaking changes

--- .config/nvim/lua/plugins/persisted.lua
+++ .config/nvim/lua/plugins/persisted.lua
@@ -13,14 +13,6 @@ return {
     config = function()
       require('persisted').setup {
           use_git_branch = true,
-          after_save = function()
-            vim.notify('Session was saved!')
-          end,
-          telescope = {
-              before_source = function()
-                vim.cmd('bufdo Bwipeout')
-              end
-          },
       }
 
       require('telescope').load_extension('persisted')
