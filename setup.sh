92916badb8234c47d50b3112fcbe3e30583a03a9
Fernando Farias <nandofarias@me.com>
2023-03-11 18:19:58 -0300

    setup: add step to install xcode command line tools

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
 if ! command -v brew &>/dev/null; then
-  echo 'Homebrew not installed, installing...'
+  echo "Homebrew not found. Installing …"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-echo 'Cloning dotfiles...'
-git clone --bare git@github.com:nandofarias/dotfiles.git $HOME/.cfg
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Command Line Tools for Xcode not found. Installing …"
+  # This temporary file prompts the "softwareupdate" utility to list the Command Line Tools
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed "s/^[^C]* //")
+  softwareupdate -i "$PROD" --verbose;
+fi
 
-echo 'Insalling brew packages...'
+DOTFILES="$HOME/.cfg"
+if [ ! -d "$DOTFILES" ]; then
+  echo "Dotfiles not found. Cloning …"
+  git clone --bare git@github.com:nandofarias/dotfiles.git $HOME/.cfg
+  git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+fi
+
+echo "Installing brew packages …"
 brew bundle
+
+echo "All good to go!"
