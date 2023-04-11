4cd432c7872e04bbbdc7c1d8a1dc65445240729b
Fernando Farias <nandofarias@me.com>
2023-04-11 11:39:29 -0300

    hammerspoon: change safari to arc

--- .hammerspoon/keymaps.lua
+++ .hammerspoon/keymaps.lua
@@ -1,18 +1,18 @@
 local function open_app(appName, appPath)
-    return function()
-        local app = hs.application.get(appName)
-        if app ~= nil and app:isFrontmost() then
-            app:hide()
-        else
-            hs.application.launchOrFocus(appPath)
-        end
+  return function()
+    local app = hs.application.get(appName)
+    if app ~= nil and app:isFrontmost() then
+      app:hide()
+    else
+      hs.application.launchOrFocus(appPath)
     end
+  end
 end
 
 hs.hotkey.bind({ "command" }, ";", open_app("Alacritty", "/Applications/Alacritty.app"))
 hs.hotkey.bind({ "command" }, ".", open_app("kitty", "/Applications/Kitty.app"))
 -- Ctrl + 1,2,3 is used to switch to different desktops
-hs.hotkey.bind({ "control" }, "4", open_app("Safari", "/Applications/Safari.app"))
+hs.hotkey.bind({ "control" }, "4", open_app("Arc", "/Applications/Arc.app"))
 hs.hotkey.bind({ "control" }, "5", open_app("Dash", "/Applications/Dash.app"))
 hs.hotkey.bind({ "control" }, "6", open_app("Slack", "/Applications/Slack.app"))
 hs.hotkey.bind({ "control" }, "7", open_app("WhatsApp", "/Applications/WhatsApp.app"))
