b3bf9d279d5edc06e77d4ea2d61221d4f46fb90d
Fernando Farias <nandofarias@me.com>
2023-12-14 16:36:34 -0300

    nvim: handpick a few ts parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,8 +12,16 @@ return {
     require "nvim-treesitter.install".compilers = { "gcc-11" }
     require('tree-sitter-just').setup {}
     require('nvim-treesitter.configs').setup({
-      ensure_installed = 'all',
-      ignore_install = { "liquidsoap" },
+      ensure_installed = { 'angular', 'arduino', 'astro', 'awk', 'bash', 'c', 'clojure', 'cmake', 'comment',
+        'commonlisp', 'cpp', 'css', 'csv', 'dart', 'diff', 'dockerfile', 'dot', 'eex', 'elixir', 'elm', 'erlang',
+        'fennel', 'fish', 'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore', 'gleam', 'go', 'gomod',
+        'gosum', 'gowork', 'gpg', 'graphql', 'hack', 'haskell', 'hcl', 'heex', 'http', 'html', 'ini',
+        'java', 'javascript', 'jsdoc', 'json', 'json5', 'jsonc', 'jsonnet', 'julia', 'kotlin', 'latex',
+        'ledger', 'lua', 'make', 'markdown', 'markdown_inline', 'mermaid', 'nix', 'ocaml', 'ocaml_interface',
+        'ocamllex', 'pascal', 'passwd', 'pem', 'perl', 'php', 'phpdoc', 'po', 'prisma', 'properties', 'proto', 'prql',
+        'psv', 'pug', 'puppet', 'pymanifest', 'python', 'query', 'r', 'regex', 'requirements', 'robot', 'ruby', 'rust',
+        'scala', 'scheme', 'scss', 'solidity', 'sparql', 'sql', 'ssh_config', 'supercollider', 'surface', 'svelte',
+        'swift', 'terraform', 'tsv', 'tsx', 'typescript', 'vim', 'vimdoc', 'vue', 'xml', 'yaml', 'zig' },
       indent = {
         enable = true
       },
@@ -25,9 +33,13 @@ return {
       autotag = {
         enable = true,
       },
-      context_commentstring = {
-        enable = true
-      },
+      disable = function(lang, buf)
+        local max_filesize = 10 * 1024 -- 10 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
       textobjects = {
         select = {
           enable = true,
