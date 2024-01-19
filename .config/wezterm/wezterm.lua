6f8a2ff574cdbc2c2cd3fa492caf23c8f0eabbb0
Fernando Farias <nandofarias@me.com>
2024-01-19 14:42:02 -0300

    migrate from kitty to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,77 +1,51 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("MonoLisa"),
-  font_size = 15,
+  font = wezterm.font("MonoLisaPlus Nerd Font"),
+  font_size = 14,
   line_height = 1.2,
 
   window_decorations = "RESIZE",
   window_close_confirmation = "NeverPrompt",
-  window_background_opacity = 1.0,
+  window_background_opacity = 0.9,
 
-  color_scheme = "Dracula PRO (Van Helsing)",
-  color_schemes = {
-    ["Dracula PRO (Van Helsing)"] = {
-      foreground = "#f8f8f2",
-      background = "#0b0d0f",
-      cursor_bg = "#f8f8f2",
-      cursor_fg = "#0b0d0f",
-      cursor_border = "#f8f8f2",
-      selection_fg = "#0b0d0f",
-      selection_bg = "#414d58",
-      scrollbar_thumb = "#414d58",
-      split = "#9580ff",
-      ansi = { "#0b0d0f", "#ff9580", "#8aff80", "#ffff80", "#9580ff", "#ff80bf", "#80ffea", "#f8f8f2" },
-      brights = { "#0b0d0f", "#ffaa99", "#a2ff99", "#ffff99", "#aa99ff", "#ff99cc", "#99ffee", "#ffffff" },
-      indexed = {
-        [136] = "#414d58"
-      },
-      compose_cursor = "#ff9580",
-      tab_bar = {
-        background = "#0b0d0f",
-        active_tab = {
-          bg_color = "#708ca9",
-          fg_color = "#f8f8f2",
-          intensity = "Normal",
-          underline = "None",
-          italic = false,
-          strikethrough = false
-        },
-        inactive_tab = {
-          bg_color = "#414d58",
-          fg_color = "#f8f8f2"
-        },
-        inactive_tab_hover = {
-          bg_color = "#708ca9",
-          fg_color = "#f8f8f2",
-          italic = true
-        },
-        new_tab = {
-          bg_color = "#0b0d0f",
-          fg_color = "#f8f8f2"
-        },
-        new_tab_hover = {
-          bg_color = "#ff80bf",
-          fg_color = "#f8f8f2",
-          italic = true
-        }
-      }
-    }
-  },
+  use_fancy_tab_bar = false,
+  force_reverse_video_cursor = true,
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  max_fps = 120,
+
+  default_cursor_style = "SteadyBar",
+
+  color_scheme = 'Tokyo Night Moon',
+
+  disable_default_key_bindings = true,
+  debug_key_events = true,
 
   keys = {
-    { key = "d", mods = "CMD",       action = wezterm.action { SendString = "\x02\x76" } },
-    { key = "d", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x73" } },
-    { key = "t", mods = "CMD",       action = wezterm.action { SendString = "\x02\x63" } },
-    { key = "w", mods = "CMD",       action = wezterm.action { SendString = "\x02\x78" } },
-    { key = "[", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x70" } },
-    { key = "]", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x6e" } },
-    { key = "`", mods = "CTRL",      action = wezterm.action { SendString = "\x02\x7a" } },
-    { key = "[", mods = "CTRL",      action = wezterm.action { SendString = "\x02\x5b" } },
-    { key = "h", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x3c" } },
-    { key = "j", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x2d" } },
-    { key = "k", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x2b" } },
-    { key = "l", mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x3e" } },
-    { key = "k", mods = "CMD",       action = wezterm.action { SendString = "\x63\x6C\x65\x61\x72\x0A" } },
+    { key = "q",     mods = "CMD",       action = wezterm.action.QuitApplication },
+    { key = "0",     mods = "CTRL",      action = wezterm.action.ResetFontSize },
+    { key = "-",     mods = "CTRL",      action = wezterm.action.DecreaseFontSize },
+    { key = "=",     mods = "CTRL",      action = wezterm.action.IncreaseFontSize },
+    { key = "n",     mods = "CMD|SHIFT", action = wezterm.action.SpawnWindow },
+    { key = "p",     mods = "CMD|SHIFT", action = wezterm.action.ActivateCommandPalette },
+    { key = "v",     mods = "CMD",       action = wezterm.action.PasteFrom("Clipboard") },
+    { key = "Copy",  mods = "NONE",      action = wezterm.action.CopyTo("Clipboard") },
+    { key = "Paste", mods = "NONE",      action = wezterm.action.PasteFrom("Clipboard") },
+    { key = "Enter", mods = "CMD",       action = wezterm.action.ToggleFullScreen },
+
+    { key = "d",     mods = "CMD",       action = wezterm.action { SendString = "\x02\x76" } },
+    { key = "d",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x73" } },
+    { key = "t",     mods = "CMD",       action = wezterm.action { SendString = "\x02\x63" } },
+    { key = "w",     mods = "CMD",       action = wezterm.action { SendString = "\x02\x78" } },
+    { key = "{",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x70" } },
+    { key = "}",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x6e" } },
+    { key = "`",     mods = "CTRL",      action = wezterm.action { SendString = "\x02\x7a" } },
+    { key = "[",     mods = "CTRL",      action = wezterm.action { SendString = "\x02\x5b" } },
+    { key = "h",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x3c" } },
+    { key = "j",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x2d" } },
+    { key = "k",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x2b" } },
+    { key = "l",     mods = "CMD|SHIFT", action = wezterm.action { SendString = "\x02\x3e" } },
+    { key = "k",     mods = "CMD",       action = wezterm.action { SendString = "\x63\x6C\x65\x61\x72\x0A" } },
   },
 }
