0aa675c683232e11b99ece490c230d8f1a94723d
Fernando Farias <nandofarias@me.com>
2024-01-26 07:00:07 -0300

    hammerspoon: change hotkeys

--- .hammerspoon/keymaps.lua
+++ .hammerspoon/keymaps.lua
@@ -9,10 +9,8 @@ local function open_app(appName, appPath)
   end
 end
 
-hs.hotkey.bind({ "command" }, ";", open_app("Alacritty", "/Applications/Alacritty.app"))
-hs.hotkey.bind({ "command" }, ".", open_app("kitty", "/Applications/Kitty.app"))
--- Ctrl + 1,2,3 is used to switch to different desktops
-hs.hotkey.bind({ "control" }, "4", open_app("Arc", "/Applications/Arc.app"))
-hs.hotkey.bind({ "control" }, "5", open_app("Dash", "/Applications/Dash.app"))
-hs.hotkey.bind({ "control" }, "6", open_app("Slack", "/Applications/Slack.app"))
-hs.hotkey.bind({ "control" }, "7", open_app("WhatsApp", "/Applications/WhatsApp.app"))
+hs.hotkey.bind({ "command" }, ".", open_app("WezTerm", "/Applications/WezTerm.app"))
+hs.hotkey.bind({ "command", "ctrl", "alt" }, "4", open_app("Arc", "/Applications/Arc.app"))
+hs.hotkey.bind({ "command", "ctrl", "alt" }, "5", open_app("Dash", "/Applications/Dash.app"))
+hs.hotkey.bind({ "command", "ctrl", "alt" }, "6", open_app("Slack", "/Applications/Slack.app"))
+hs.hotkey.bind({ "command", "ctrl", "alt" }, "7", open_app("WhatsApp", "/Applications/WhatsApp.app"))
