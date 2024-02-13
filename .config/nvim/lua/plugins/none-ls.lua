8dccfe2a6ea0f796910d73d1fcafbdb537f66052
Fernando Farias <nandofarias@me.com>
2024-02-13 16:56:39 -0300

    nvim: minor changes to nonels

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,29 +6,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettierd.with({
-          extra_args = {
-            "--arrow-parens avoid",
-          },
-          filetypes = {
-            "markdown",
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "css",
-            "scss",
-            "less",
-            "html",
-            "json",
-            "yaml",
-            "vue",
-            "markdown",
-            "markdown.mdx",
-            "graphql",
-            "handlebars",
-          },
-        }),
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.credo,
         null_ls.builtins.formatting.erlfmt,
         null_ls.builtins.diagnostics.fish,
