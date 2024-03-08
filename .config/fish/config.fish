c1403e207b6fbd078a4e6b037a14311d9d154b81
Fernando Farias <nandofarias@me.com>
2024-03-08 16:51:10 -0300

    fish: add homebrew vendor completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,6 +17,12 @@ if status is-interactive
   if [ -s "/usr/local/bin/brew" ];
     eval "$(/usr/local/bin/brew shellenv)"
   end
+  if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+  end
+  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+  end
 
   # Fix curl
   set -gx HOMEBREW_FORCE_BREWED_CURL 1
