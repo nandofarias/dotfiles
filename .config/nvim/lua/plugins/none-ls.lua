915e97764676b1cff6367f2a1bbe209d34349ca9
Fernando Farias <nandofarias@me.com>
2024-03-07 21:19:00 -0300

    nvim: add none-ls extras

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,11 +1,14 @@
 return {
   'nvimtools/none-ls.nvim',
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require('null-ls')
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.credo,
         null_ls.builtins.formatting.erlfmt,
