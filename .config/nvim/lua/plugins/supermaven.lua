196af55f09df2ae9d8ad78087fca29bb5c8067a9
Fernando Farias <nandofarias@me.com>
2024-06-21 13:27:31 -0300

    nvim: config supermaven

--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,6 +1,10 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup({})
+    require('supermaven-nvim').setup({
+      log_level = 'off',
+      disable_inline_completion = true,
+      disable_keymaps = true,
+    })
   end,
 }
