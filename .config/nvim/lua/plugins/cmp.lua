a0a9ce2d54fe957a08b55f61052275a06041ec3e
Fernando Farias <nandofarias@me.com>
2024-07-02 11:11:40 -0300

    nvim: replace copilot for supermaven

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,41 +1,40 @@
 return {
-  'hrsh7th/nvim-cmp',
+  "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    'onsails/lspkind.nvim',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'andersevenrud/cmp-tmux',
+    "onsails/lspkind.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "andersevenrud/cmp-tmux",
   },
   config = function()
-    vim.g.vsnip_snippet_dir = '~/.config/nvim/snippets'
+    vim.g.vsnip_snippet_dir = "~/.config/nvim/snippets"
 
-    local cmp = require('cmp')
-    local lspkind = require('lspkind')
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
 
-    cmp.setup {
+    cmp.setup({
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       },
       formatting = {
-        fields = { 'kind', 'abbr', 'menu' },
+        fields = { "kind", "abbr", "menu" },
         format = lspkind.cmp_format({
           symbol_map = {
             Supermaven = "",
-            Copilot = ""
           },
           before = function(entry, vim_item)
             vim_item.menu = ({
-              buffer = ' Buffer',
+              buffer = " Buffer",
               nvim_lsp = vim_item.kind,
-              path = ' Path',
-              luasnip = ' LuaSnip',
+              path = " Path",
+              luasnip = " LuaSnip",
             })[entry.source.name]
 
             return vim_item
@@ -48,43 +47,42 @@ return {
         end,
       },
       mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-y>'] = cmp.mapping.close(),
-        ['<C-c>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-y>"] = cmp.mapping.close(),
+        ["<C-c>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
       },
       sources = {
-        { name = 'supermaven' },
-        { name = 'copilot' },
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'nvim_lua' },
-        { name = 'vsnip' },
-        { name = 'path' },
-        { name = 'tmux' },
+        { name = "supermaven" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lua" },
+        { name = "vsnip" },
+        { name = "path" },
+        { name = "tmux" },
         {
-          name = 'buffer',
+          name = "buffer",
           option = {
             get_bufnrs = function()
               return vim.api.nvim_list_bufs()
-            end
-          }
+            end,
+          },
         },
-      }
-    }
+      },
+    })
 
     local opts = { remap = true, expr = true, replace_keycodes = false }
     -- vim.keymap.set({'i','s'}, '<C-J>',   "vsnip#expandable() ? '<Plug>(vsnip-expand)'         : '<C-J>'",   opts)
     -- vim.keymap.set({'i','s'}, '<C-L>',   "vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-L>'",   opts)
-    vim.keymap.set({ 'i', 's' }, '<Tab>', "vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", opts)
-    vim.keymap.set({ 'i', 's' }, '<S-Tab>', "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", opts)
+    vim.keymap.set({ "i", "s" }, "<Tab>", "vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", opts)
+    vim.keymap.set({ "i", "s" }, "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", opts)
 
-    vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", {fg ="#6CC644"})
-  end
+    vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
+  end,
 }
--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,8 +1,8 @@
 return {
-  'supermaven-inc/supermaven-nvim',
+  "supermaven-inc/supermaven-nvim",
   config = function()
-    require('supermaven-nvim').setup({
-      log_level = 'off',
+    require("supermaven-nvim").setup({
+      log_level = "off",
       disable_inline_completion = true,
       disable_keymaps = true,
     })
