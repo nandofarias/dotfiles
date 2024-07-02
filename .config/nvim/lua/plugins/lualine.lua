528812500fcc94a99095280b2bb2d8a9a45e980c
Fernando Farias <nandofarias@me.com>
2024-07-02 18:55:05 -0300

    nvim: fix tokyonight/lualine bug

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,10 @@
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   config = function()
-    local tokyonight = require('lualine.themes.tokyonight')
-    tokyonight.normal.c.bg = 'none'
+    local tokyonight = require("lualine.themes.tokyonight-moon")
+    tokyonight.normal.c.bg = "none"
 
-    require('lualine').setup {
+    require("lualine").setup({
       options = {
         theme = tokyonight,
         disabled_filetypes = {
@@ -13,28 +13,33 @@ return {
           "TelescopePrompt",
           "TelescopeResults",
         },
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
         globalstatus = true,
       },
       sections = {
         lualine_a = {
-          { 'mode', separator = { left = '' }, right_padding = 2 },
+          { "mode", separator = { left = "" }, right_padding = 2 },
         },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename', function() return require('lsp-progress').progress() end },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          "filename",
+          function()
+            return require("lsp-progress").progress()
+          end,
+        },
         lualine_x = {
           {
-            require('lazy.status').updates,
-            cond = require('lazy.status').has_updates,
-            color = { bg = 'none', fg = '#ff966c' },
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { bg = "none", fg = "#ff966c" },
           },
         },
-        lualine_y = { 'filetype', 'progress' },
+        lualine_y = { "filetype", "progress" },
         lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
+          { "location", separator = { right = "" }, left_padding = 2 },
         },
       },
-    }
-  end
+    })
+  end,
 }
