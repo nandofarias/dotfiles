6246ac232305cd30fcb545c48463e5183889e0f3
Fernando Farias <nandofarias@me.com>
2024-08-22 15:06:21 -0300

    nvim: fix annoying consistency implemented by the plugin, I WANT MY VIM EXPERIENCE WITH BUGS PLZ

--- .config/nvim/lua/plugins/spider.lua
+++ .config/nvim/lua/plugins/spider.lua
@@ -5,11 +5,10 @@ return {
     { "e",  function() require('spider').motion('e') end,  desc = "Spider-e",  mode = { "n", "o", "x" } },
     { "b",  function() require('spider').motion('b') end,  desc = "Spider-b",  mode = { "n", "o", "x" } },
     { "ge", function() require('spider').motion('ge') end, desc = "Spider-ge", mode = { "n", "o", "x" } },
+    { "w",  function() require('spider').motion('w') end,  mode = { "o" } },
+    { "cw", "c<cmd>lua require('spider').motion('e')<CR>" },
+    { "dw", "c<cmd>lua require('spider').motion('e')<CR>" },
   },
   lazy = true,
-  config = function()
-    require("spider").setup({
-      skipInsignificantPunctuation = false,
-    })
-  end
+  config = true
 }
