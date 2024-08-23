06d3475cfbd1afbc81056ce938488e0013496d94
Fernando Farias <nandofarias@me.com>
2024-08-23 13:59:24 -0300

    Fix focus and trouble

--- .config/nvim/lua/plugins/focus.lua
+++ .config/nvim/lua/plugins/focus.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-focus/focus.nvim',
+  lazy = false,
   version = '*',
   keys = {
     { '<leader>sn', function() require('focus').split_nicely() end, { desc = 'split nicely' } }
--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,12 +1,37 @@
 return {
   'folke/trouble.nvim',
+  cmd = 'Trouble',
   config = true,
   keys = {
-    { '<leader>xx', '<cmd>TroubleToggle<cr>', desc = 'Trouble Toggle' },
-    { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Trouble Workspace' },
-    { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Trouble Document' },
-    { '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', desc = 'Trouble Quickfix' },
-    { '<leader>xl', '<cmd>TroubleToggle loclist<cr>', desc = 'Trouble Loclist' },
-    { '<leader>xr', '<cmd>TroubleToggle lsp_references<cr>', desc = 'Trouble LSP' },
-  }
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>xl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
