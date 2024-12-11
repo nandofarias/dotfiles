664f63c5780cfaa1356bc46d51109fc36212db22
Fernando Farias <nandofarias@me.com>
2024-12-11 13:18:33 -0300

    minor tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -98,8 +98,8 @@ if status is-interactive
   # Import GPG keys
   set -gx GPG_TTY (tty)
 
-  # iTerm
-  test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+  # Fzf
+  fzf --fish | source
 
   # Abbreviations
   abbr -ag mps "iex -S mix phx.server"
