e083c9ab2f5d544b70b3426389c6d20b986d5c38
Fernando Farias <nandofarias@me.com>
2024-12-27 16:34:16 -0300

    nvim: fix annoying stuff in dadbod ui

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -3,23 +3,17 @@ return {
   cmd = "DBUI",
   dependencies = {
     'kristijanhusak/vim-dadbod-ui',
-    'kristijanhusak/vim-dadbod-completion',
   },
   keys = {
     { '<leader>do', ':DBUI<CR><CR>',  desc = "Database UI Open" },
     { '<leader>dc', ':DBUIClose<CR>', desc = "Database UI Close" },
   },
   config = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_force_echo_notifications = 1
     vim.opt.cmdheight = 1
     vim.g.db_ui_show_help = 0
     vim.g.db_ui_win_position = 'left'
     vim.g.db_ui_tmp_query_location = '~/Developer/queries'
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'sql', 'mysql', 'plsql' },
-      callback = function()
-        require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
-      end
-    })
   end
 }
