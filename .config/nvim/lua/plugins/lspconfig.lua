90ba284530bd73967fb50a1d9904bdef0cab62a5
Fernando Farias <nandofarias@me.com>
2024-12-27 13:39:48 -0300

    nvim: removing umbrella support for lexical and elixirls (should be default, hate to maintain this)

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -119,21 +119,6 @@ return {
             suggestSpecs = true
           }
         }
-        opts.root_dir = function(fname)
-          local path = lspconfig.util.path
-          local child_or_root_path = lspconfig.util.root_pattern({ "mix.exs", ".git" })(fname)
-          local maybe_umbrella_path = lspconfig.util.root_pattern({ "mix.exs" })(
-            vim.loop.fs_realpath(path.join({ child_or_root_path, ".." }))
-          )
-
-          local has_ancestral_mix_exs_path = vim.startswith(child_or_root_path,
-            path.join({ maybe_umbrella_path, "apps" }))
-          if maybe_umbrella_path and not has_ancestral_mix_exs_path then
-            maybe_umbrella_path = nil
-          end
-
-          return maybe_umbrella_path or child_or_root_path or vim.loop.os_homedir()
-        end
 
         lspconfig.elixirls.setup(opts)
       end,
@@ -143,21 +128,6 @@ return {
           default_config = {
             filetypes = { "elixir", "eelixir", "heex" },
             cmd = { "$HOME/.local/share/nvim/mason/packages/lexical/libexec/lexical/bin/start_lexical.sh" },
-            root_dir = function(fname)
-              local path = lspconfig.util.path
-              local child_or_root_path = lspconfig.util.root_pattern({ "mix.exs", ".git" })(fname)
-              local maybe_umbrella_path = lspconfig.util.root_pattern({ "mix.exs" })(
-                vim.loop.fs_realpath(path.join({ child_or_root_path, ".." }))
-              )
-
-              local has_ancestral_mix_exs_path = vim.startswith(child_or_root_path,
-                path.join({ maybe_umbrella_path, "apps" }))
-              if maybe_umbrella_path and not has_ancestral_mix_exs_path then
-                maybe_umbrella_path = nil
-              end
-
-              return maybe_umbrella_path or child_or_root_path or vim.loop.os_homedir()
-            end,
             settings = {}
           },
         })
