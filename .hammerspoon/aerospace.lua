b678f827cd37292e545c46144e855341a01aa27f
Fernando Farias <nandofarias@me.com>
2024-12-31 13:14:45 -0300

    nvim: optmize swipe

--- .hammerspoon/aerospace.lua
+++ .hammerspoon/aerospace.lua
@@ -6,20 +6,20 @@ local function aerospaceExec(cmd)
   os.execute("nohup " .. AEROSPACE .. " " .. cmd .. " &")
 end
 
--- use four finger swipe to switch workspace
+-- use three finger swipe to switch workspace
 local current_id, threshold
-Swipe:start(4, function(direction, distance, id)
+Swipe:start(3, function(direction, distance, id)
   if id == current_id then
     if distance > threshold then
       threshold = math.huge -- only trigger once per swipe
       if direction == "left" then
-        aerospaceExec("workspace --wrap-around prev")
-      elseif direction == "right" then
         aerospaceExec("workspace --wrap-around next")
+      elseif direction == "right" then
+        aerospaceExec("workspace --wrap-around prev")
       end
     end
   else
     current_id = id
-    threshold = 0.1 -- swipe distance > 10% of trackpad
+    threshold = 0.04 -- swipe distance > 4% of trackpad
   end
 end)
