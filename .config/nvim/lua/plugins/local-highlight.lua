81be5af02f4b70d1b30f435dfa72384c5939e096
Fernando Farias <nandofarias@me.com>
2025-02-25 15:00:40 -0300

    nvim: update deps

--- .config/nvim/lua/plugins/local-highlight.lua
+++ .config/nvim/lua/plugins/local-highlight.lua
@@ -1,5 +1,30 @@
 vim.cmd [[hi TSDefinitionUsage guibg=#565f89]]
 return {
-    'tzachar/local-highlight.nvim',
-    config = true
+  'tzachar/local-highlight.nvim',
+  dependencies = {
+    {
+      "folke/snacks.nvim",
+      priority = 1000,
+      lazy = false,
+      -- ---@type snacks.Config
+      -- opts = {
+      --   -- your configuration comes here
+      --   -- or leave it empty to use the default settings
+      --   -- refer to the configuration section below
+      --   bigfile = { enabled = true },
+      --   dashboard = { enabled = true },
+      --   explorer = { enabled = true },
+      --   indent = { enabled = true },
+      --   input = { enabled = true },
+      --   picker = { enabled = true },
+      --   notifier = { enabled = true },
+      --   quickfile = { enabled = true },
+      --   scope = { enabled = true },
+      --   scroll = { enabled = true },
+      --   statuscolumn = { enabled = true },
+      --   words = { enabled = true },
+      -- },
+    }
+  },
+  config = true
 }
