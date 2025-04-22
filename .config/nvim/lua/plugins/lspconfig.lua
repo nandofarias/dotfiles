6bccabfac95546a2fbc05d4e0c696ca7c977419c
Fernando Farias <nandofarias@me.com>
2025-04-22 11:59:38 -0300

    nvim: fix lexical

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -113,18 +113,17 @@ return {
 
       ['lexical'] = function()
         lspconfig.lexical.setup({
-          default_config = {
-            -- cmd = { "$HOME/.local/share/nvim/mason/packages/lexical/libexec/lexical/bin/start_lexical.sh" },
-            filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
-            root_dir = function(fname)
-              local matches = vim.fs.find({ 'mix.exs' }, { upward = true, limit = 2, path = fname })
-              local child_or_root_path, maybe_umbrella_path = unpack(matches)
-              local root_dir = vim.fs.dirname(maybe_umbrella_path or child_or_root_path)
+          -- cmd = { "$HOME/.local/share/nvim/mason/packages/lexical/libexec/lexical/bin/start_lexical.sh" },
+          filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
+          root_dir = function(fname)
+            local matches = vim.fs.find({ 'mix.exs' }, { upward = true, limit = 2, path = fname })
+            local child_or_root_path, maybe_umbrella_path = unpack(matches)
+            local root_dir = vim.fs.dirname(maybe_umbrella_path or child_or_root_path)
 
-              return root_dir
-            end,
-            single_file_support = true,
-          },
+            return root_dir
+          end,
+          single_file_support = true,
+          dialyzer_enabled = true
         })
       end,
 
