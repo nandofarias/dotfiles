4be6e2e83df423bf5836be8eadf194d4f3fac9fb
Fernando Farias <nandofarias@me.com>
2025-04-22 11:59:24 -0300

    nvim: fix blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -23,12 +23,25 @@ return {
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full 'keymap' documentation for information on defining your own keymap.
-      keymap = { preset = 'enter' },
+      keymap = {
+        preset = 'enter',
+        ["<CR>"] = {
+          function(cmp)
+            local ok, supermaven = pcall(require, "supermaven-nvim.completion_preview")
+            if ok and supermaven.has_suggestion() then
+              vim.schedule(supermaven.on_accept_suggestion)
+              return true
+            end
+          end,
+          "accept",
+          "fallback",
+        },
+      },
       enabled = function()
-        return (vim.bo.buftype ~= "prompt" and
-              vim.bo.buftype ~= "NvimTree" and
-              vim.bo.buftype ~= "TelescopePrompt" and
-              vim.bo.filetype ~= "DressingInput") or
+        return vim.bo.buftype ~= "prompt" and
+            vim.bo.buftype ~= "NvimTree" and
+            vim.bo.buftype ~= "TelescopePrompt" and
+            vim.bo.filetype ~= "DressingInput" and
             vim.b.completion ~= false
       end,
       signature = {
