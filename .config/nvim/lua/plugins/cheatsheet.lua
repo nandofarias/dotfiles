f177ce0877548fd6c9f762327ee2e78b869838e6
Fernando Farias <nandofarias@me.com>
2026-02-17 19:32:13 -0300

    nvim: add cheatsheet.nvim

---
+++ .config/nvim/lua/plugins/cheatsheet.lua
@@ -0,0 +1,11 @@
+return {
+  'sudormrfbin/cheatsheet.nvim',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/popup.nvim',
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<space>?', '<cmd>Cheatsheet<cr>', desc = 'Cheatsheet' },
+  },
+}
